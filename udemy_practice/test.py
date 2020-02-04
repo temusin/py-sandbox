a2a6327ed89de7be7ed40e166f563c924ad04f92
Kitae Kim <kkt9595@gmail.com>
2020-02-03 22:09:10 -0500

    Adding more codes in python

--- udemy_practice/test.py
+++ udemy_practice/test.py
@@ -1,4 +1,30 @@
 class Test:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        print(self.name, "is speaking.")
+
+
+def main():
+    name = Test("Kitae")
+    name.speak()
+    print('This is a string {}'.format('Inserted!!'))
+    print('The {} {} {}'.format('fox', 'brown', 'quick'))
+    print('The {0} {0} {0}'.format('fox', 'brown', 'quick'))
+    print('The {q} {b} {f}'.format(f='fox', b='brown', q='quick'))
+    result = 100/77
+    print("The result was {}".format(result))
+
+    # Float formatting follows "{value:width.precision f}"
+    print("The result was {r:10.3f}".format(r=result))
+    print("The result was {r:1.5f}".format(r=result))
+    print("The result was {r:1.2f}".format(r=result))
+
+    # f literal methods.
+    first_name = 'Kitae'
+    print(f'Hello, his name is {first_name}')
+
+
+if __name__ == '__main__':
+    main()
